00992e6731d26b53bcfbd5e65dd1c913ffb92ddd
JustWork Robot <robot@just-work.org>
2023-03-02 15:06:46 +0300

    enable prod

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -1,5 +1,9 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(pr6$=k)5w28rdp*@q_u!@lw6jpx_x$a@pcpgmv#th_aiqo#pb'
